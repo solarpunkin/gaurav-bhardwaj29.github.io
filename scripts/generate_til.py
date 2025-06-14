68be30f3891953a67022cdc1ea13aa770feea5c7
Gaurav Bhardwaj <98477295+gaurav-bhardwaj29@users.noreply.github.com>
2025-06-14 05:17:07 +0000

    Added 2025-06-14-github-actions-til

--- scripts/generate_til.py
+++ scripts/generate_til.py
@@ -170,7 +170,7 @@ for i, post in enumerate(posts):
       <body>
         <h1>{post['title']}</h1>
         <div class="til-body">{post['body']}</div>
-        <div class="til-date">Posted on {now_ist}</div>· Follow me on <a href="https://x.com/bardgaurav">Twitter</a>.
+        <div class="til-date">Posted on {now_ist} · Follow me on <a href="https://x.com/bardgaurav">Twitter</a>.</div>
         <ul class="til-list">
           <li><a href="../../index.html">← TIL</a></li>
         </ul>
